774c5244649864f49358f8313e88c81c9ea61100
Ch Bhuvaneswari <123800444+Bhuvaneswari505@users.noreply.github.com>
2025-03-01 23:31:29 +0530

    Add files via upload

---
+++ jsscript.js
@@ -0,0 +1,13 @@
+const input=document.querySelector("input");
+const btn = document.getElementById("btn");
+const icon = document.querySelector(".icon");
+const weather = document.querySelector("weather");
+const temperature = document.querySelector("temperature");
+const description = document.querySelector("description");
+
+
+
+
+const apikey='b6d92195ca17987f40258d337a557190'
+
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
